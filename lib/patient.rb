2d586decf827b8b283f65f9c50d0a902dfca3ae8
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2018-08-23 05:22:14 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,18 +1,30 @@
-class Patient
-   attr_reader :name, :appointment, :doctor
-   def initialize(name)
+	class Patient
+  attr_accessor :name, :appointments
+  
+  @@all =[]
+    
+  def self.all       
+   @@all
+  end
+  
+  def new_appointment(doctor,date)
+  appointment = Appointment.new(self,doctor,date)
+  @appointments << appointment
+  
+  end
+  
+  def initialize(name)
     @name = name
+    @appointments = []
+    
   end
-   def new_appointment(doctor, date)
-    Appointment.new(self, doctor, date)
+   def add_appointment(appointment)
+    @appointments << appointment
+    appointment.name = self
   end
-   def appointments
-    Appointment.all.select do |appointment|
-      appointment.patient == self
-    end
-  end
+  
    def doctors
-    Appointment.all.map do |appointment|
+    self.appointments.collect do |appointment|
       appointment.doctor
     end
   end
