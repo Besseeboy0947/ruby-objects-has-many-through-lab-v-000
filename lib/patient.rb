88bbfe42edeb08dcf0cac0a0ca67c18363741c29
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2018-08-23 05:22:19 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,12 +1,6 @@
 	class Patient
   attr_accessor :name, :appointments
   
-  @@all =[]
-    
-  def self.all       
-   @@all
-  end
-  
   def new_appointment(doctor,date)
   appointment = Appointment.new(self,doctor,date)
   @appointments << appointment
